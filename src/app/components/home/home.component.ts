7195f241855e97d7a17b037f2ca7351c347b6b98
Mayank Kaushal <pixelawakes@gmail.com>
2025-04-02 00:38:35 +0530

    fix: update home page CTA button and add Font Awesome icons

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,40 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
-import { filter, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [RouterModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-  isLoggedIn = false;
-
+export class HomeComponent {
   constructor(
-    private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) {}
 
-  ngOnInit() {
-    this.authService.isInitialized().pipe(
-      filter(initialized => initialized),
-      take(1)
-    ).subscribe(() => {
-      this.authService.getCurrentUser().subscribe(user => {
-        this.isLoggedIn = !!user;
-      });
-    });
-  }
-
   handleReportClick() {
-    if (this.isLoggedIn) {
-      this.router.navigate(['/report']);
-    } else {
-      this.router.navigate(['/login'], { queryParams: { returnUrl: '/report' } });
-    }
+    this.authService.getCurrentUser().subscribe(user => {
+      if (user) {
+        this.router.navigate(['/report']);
+      } else {
+        this.router.navigate(['/login'], { queryParams: { returnUrl: '/report' } });
+      }
+    });
   }
 } 
